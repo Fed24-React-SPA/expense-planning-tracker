f62494d91c04789155cf9a6867aaa52d99a522d8
Marija Rubil <rubilmarija@gmail.com>
2022-12-28 14:45:44 +0100

    Add Button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import AddButton from "./Components/AddButton/AddButton";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>React App Group 5 Project</h1>
+			<AddButton />
 		</div>
 	);
 }
