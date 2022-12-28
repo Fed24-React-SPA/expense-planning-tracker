32750fe9b79a2a07557b3e135de6108f94143686
Marija Rubil <rubilmarija@gmail.com>
2022-12-28 18:57:11 +0100

    Navbar Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import AddButton from "./Components/AddButton/AddButton";
+import NavbarFooter from "./Components/NavbarFooter/NavbarFooter";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>React App Group 5 Project</h1>
 			<AddButton />
+			<NavbarFooter />
 		</div>
 	);
 }
