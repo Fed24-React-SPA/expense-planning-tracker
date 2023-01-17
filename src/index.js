489ce81b8a2f7ffd08bc09c6500edfc059380a0f
Amin.Haghi <90243818+AminFED23STO@users.noreply.github.com>
2023-01-18 00:26:19 +0100

    install react snap -improve seo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+import { hydrate, render } from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+    hydrate(<App />, rootElement);
+} else {
+    render(<App />, rootElement);
+}
+registerServiceWorker();
